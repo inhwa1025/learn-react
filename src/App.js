13d7309964f5da2df68c09c8c0fc69b42eeb548e
inhwa1025 <inhwa9901@gmail.com>
2022-02-08 17:16:50 +0900

    create

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Subject from './components/Subject';
 import TOC from './components/TOC';
-import Content from './components/Content';
+import ReadContent from './components/ReadContent';
+import CreateContent from './components/CreateContent';
+import Control from './components/Control';
 import './App.css';
 
 class App extends Component{
   constructor(props){
     super(props);
+    this.max_content_id = 3;
     this.state = {
       mode: 'read',
       selected_content_id:2,
@@ -20,11 +23,11 @@ class App extends Component{
     }
   }
   render() {
-    console.log('App render');
-    var _title, _desc = null;
+    var _title, _desc, _article = null;
     if(this.state.mode === 'welcome'){
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
     } else if(this.state.mode === 'read'){
       var i = 0;
       while(i < this.state.contents.length){
@@ -36,6 +39,29 @@ class App extends Component{
         }
         i = i + 1;
       }
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+    } else if(this.state.mode === 'create'){
+      _article = <CreateContent onSubmit={function(_title, _desc){
+        // add content to this.state.contents
+        this.max_content_id = this.max_content_id + 1;
+        // this.state.contents.push({
+        //   id:this.max_content_id, title:_title, desc:_desc
+        // }); 
+        // push는 원본을 수정하고, concat은 복제본을 수정해서 반환. 
+        // 큰 프로그램에서 나중에 성능 향상을 위해 concat을 쓰는 것이 좋다.
+        // => TOC.js의 shouldComponentUpdate 함수를 통해 성능 향상 구현.
+        var _contents = this.state.contents.concat(
+          {id:this.max_content_id, title:_title, desc:_desc}
+        );
+        // push를 쓰고싶다면 Array.from을 사용해 복제한 후 push 가능. 배열의 경우에만 가능
+        // 객체에 쓰고싶다면 var b = Object.assigin({}, a); 사용
+        // var newContents = Array.from(this.state.contents);
+        // newContents.push({id:this.max_content_id, title:_title, desc:_desc});
+        // this.setState({contents:newContents});
+        this.setState({
+          contents:_contents
+        });
+      }.bind(this)}></CreateContent>
     }
     return (
       <div className="App">
@@ -56,7 +82,12 @@ class App extends Component{
           }.bind(this)}
           data={this.state.contents}
         ></TOC>
-        <Content title={_title} desc={_desc}></Content>
+        <Control onChangeMode={function(_mode){
+          this.setState({
+            mode:_mode
+          });
+        }.bind(this)}></Control>
+        {_article}
       </div>
     );
   }
