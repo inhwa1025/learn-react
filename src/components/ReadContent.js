70d91f2ce3c5731544db0ae98ad740cafc2a20e9
inhwa1025 <inhwa9901@gmail.com>
2022-02-09 09:35:40 +0900

    Fix typo

--- src/components/ReadContent.js
+++ src/components/ReadContent.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 
 class ReadContent extends Component {
-    render() {
+  render() {
+    console.log('ReadContent render')
       return (
         <article>
           <h2>{this.props.title}</h2>
           {this.props.desc}
         </article>
       );
-    }
   }
+}
 
 export default ReadContent;
