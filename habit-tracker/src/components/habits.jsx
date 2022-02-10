57c28be82aa62a5beb5434e189a4293b62e425aa
inhwa1025 <inhwa9901@gmail.com>
2022-02-10 16:18:24 +0900

    Modify habit component

--- habit-tracker/src/components/habits.jsx
+++ habit-tracker/src/components/habits.jsx
@@ -2,49 +2,21 @@ import React, { Component } from 'react';
 import Habit from './habit';
 
 class Habits extends Component {
-    state = {
-        habits: [
-            {id: 1, name: 'Reading', count: 0},
-            {id: 2, name: 'Running', count: 0},
-            {id: 3, name: 'Coding', count: 0},
-        ],
-    };
-
-    handleIncrement = (habit) => {
-        const habits = [...this.state.habits];
-        const index = habits.indexOf(habit);
-        habits[index].count++;
-        this.setState({habits});    // 이름이 동일하므로 생략 가능. 사실상 {habits: habits}랑 동일.
-    };
-
-    handleDecrement = (habit) => {
-        const habits = [...this.state.habits];
-        const index = habits.indexOf(habit);
-        const count = habits[index].count - 1;
-        habits[index].count = count < 0 ? 0 : count;
-        this.setState({habits});
-    };
-
-    handleDelete = (habit) => {
-        const habits = this.state.habits.filter(item => item.id !== habit.id);
-        this.setState({habits});
-    };
-
     render() {
-        return (
-            <ul>
-                {this.state.habits.map(habit => (
-                    <Habit 
-                        key={habit.id} 
-                        habit={habit} 
-                        onIncrement={this.handleIncrement}
-                        onDecrement={this.handleDecrement}
-                        onDelete={this.handleDelete}
-                    />
-                ))}
-            </ul>
-        );
-    }
+    return (
+      <>
+        {this.props.habits.map(habit => (
+          <Habit 
+            key={habit.id} 
+            habit={habit} 
+            onIncrement={this.props.onIncrement}
+            onDecrement={this.props.onDecrement}
+            onDelete={this.props.onDelete}
+          />
+        ))}
+      </>
+    );
+  }
 }
 
 export default Habits;
