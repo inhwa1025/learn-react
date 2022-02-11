6603df3bc2baeb390e50384582787ecc601f3436
inhwa1025 <inhwa9901@gmail.com>
2022-02-11 15:25:08 +0900

    FEAT: habit component

--- practice-habit-tracker/src/components/habit.jsx
+++ practice-habit-tracker/src/components/habit.jsx
@@ -2,10 +2,37 @@ import React, { Component } from 'react';
 
 class Habit extends Component {
   render() {
+    console.log('habit');
+    const {name, count} = this.props.habit;
     return (
-      <div>
-        
-      </div>
+      <li className="habit">
+        <span className="habit-name">{name}</span>
+        <span className="habit-count">{count}</span>
+        <button 
+          className="habit-button habit-increase"
+          onClick={function(){
+            this.props.onIncrement(this.props.habit);
+          }.bind(this)}
+        >
+          <i className="fa-solid fa-square-plus"></i>
+        </button>
+        <button 
+          className="habit-button habit-decrease"
+          onClick={function(){
+            this.props.onDecrement(this.props.habit);
+          }.bind(this)}
+        >
+          <i className="fa-solid fa-square-minus"></i>
+        </button>
+        <button 
+          className="habit-button habit-delete"
+          onClick={function(){
+            this.props.onDelete(this.props.habit)
+          }.bind(this)}
+        >
+        <i className="fa-solid fa-trash-can"></i>
+        </button>
+      </li>
     );
   }
 }
